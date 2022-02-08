49c638ecd657d4630c0f95ddb22dfdc44250c967
Manikiran004 <kiranmani004@gmail.com>
2022-02-08 15:25:58 +0530

    palindrome and structarray

---
+++ structarray/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+public struct Cars
+{
+    public string model;
+    public int year;
+    public void set(string model,int year)
+    {
+        this.model = model;
+        this.year = year;
+    }
+}
+namespace structarray
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("no of cars");
+            int n = int.Parse(Console.ReadLine());
+            Cars[] car = new Cars[n];
+            for(int i = 0; i < n; i++)
+            {
+                car[i] = new Cars();
+            }
+            int min = 200000;
+            int max = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("enter car {0} details", i);
+                string s = Console.ReadLine();
+                int y = int.Parse(Console.ReadLine());
+                if (y > max)
+                {
+                    max = y;
+                }
+                if (y < min)
+                {
+                    min = y;
+                }
+                car[i].model=s;
+                car[i].year = y;
+            }
+            for (int i = min; i <=max; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    if (car[j].year == i)
+                    {
+                        Console.WriteLine("{0}::{1}", car[j].model, car[j].year);
+                    }
+                }
+               
+            }
+        }
+    }
+}
---
+++ structarray/palindrome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace structarray
+{
+    class palindrome
+    {
+        static bool palin(string s,int i,int j)
+        {
+            if (i>=j)
+            {
+                return true;
+            }
+            else
+            {
+                return s[i]==s[j] && palin(s, i + 1, j - 1);
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            if (palin(s,0,s.Length-1))
+            {
+                Console.WriteLine("palindrome");
+            }
+            else
+            {
+                Console.WriteLine("not palindrome");
+            }
+        }
+    }
+}
