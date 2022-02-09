f93985fb558ae71a91f0ab42d26aba371b6e973e
Manikiran004 <kiranmani004@gmail.com>
2022-02-09 12:04:43 +0530

    collections

---
+++ Collections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> d = new Dictionary<int, int>();
+            int[] a = { 1, 2, 1, 2, 3, 4, 1, 2 };
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (d.ContainsKey(a[i]))
+                {
+                    d[a[i]] = d[a[i]] + 1;
+                }
+                else
+                {
+                    d[a[i]] = 1;
+                }
+
+            }
+            foreach (var item in d)
+            {
+                Console.WriteLine(item.Key + "\t" + item.Value);
+            }
+        }
+    }
+}
