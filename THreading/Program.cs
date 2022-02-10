22ec45aaf92b12b684e306c5f3db80139889a1e5
Manikiran004 <kiranmani004@gmail.com>
2022-02-10 11:31:48 +0530

    threading

---
+++ THreading/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+namespace THreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(Run);
+            t.Start();
+            t.Join();
+            Console.WriteLine("iN main method");
+        }
+        static void Run()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("running");
+            }
+        }
+    }
+}
