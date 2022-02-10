cf5231a61d45475b1402f3661992736aa695c6b4
Manikiran004 <kiranmani004@gmail.com>
2022-02-10 12:37:51 +0530

    threading

--- THreading/Program.cs
+++ THreading/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 namespace THreading
 {
@@ -6,16 +7,31 @@ namespace THreading
     {
         static void Main(string[] args)
         {
-            Thread t = new Thread(Run);
-            t.Start();
-            t.Join();
-            Console.WriteLine("iN main method");
+            Stopwatch stwatch = new Stopwatch();
+            stwatch.Start();
+
+            Thread oThread = new Thread(ProcessSleep);
+            oThread.Start();
+            oThread.Join();
+            stwatch.Stop();
+            TimeSpan ts = stwatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}", ts.Hours, ts.Minutes, ts.Seconds);
+            Console.WriteLine(elapsedTime);
+            Console.WriteLine("WOrk completed");
         }
         static void Run()
         {
             for(int i = 0; i < 3; i++)
             {
                 Console.WriteLine("running");
+            }
+        }
+        static void ProcessSleep()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Work in progress");
+                Thread.Sleep(4000);//4 second
             }
         }
     }
