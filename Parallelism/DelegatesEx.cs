015f993f0dc9db21cbb2ba13e63bc646d125bf0d
Manikiran004 <kiranmani004@gmail.com>
2022-02-11 12:50:44 +0530

    delegates

--- Parallelism/DelegatesEx.cs
+++ Parallelism/DelegatesEx.cs
@@ -5,16 +5,37 @@ using System.Text;
 namespace Parallelism
 {
     public delegate void Add(int a, int b);
+    public delegate void Mul(int a, int b);
+    public delegate void Sub(int a, int b);
+    public delegate void Div(int a, int b);
     class DelegatesEx
     {
         public static void Add1(int a,int b)
         {
-            Console.WriteLine(a + b);
+            Console.WriteLine("Addition is"+(a + b));
         }
-        static void Main(string[] args)
+        public static void Mul1(int a, int b)
+        {
+            Console.WriteLine("Multiplication"+(a * b));
+        }
+        public static void Sub1(int a, int b)
+        {
+            Console.WriteLine("Subtractioj="+(a -b));
+        }
+        public static void Div1(int a, int b)
+        {
+            Console.WriteLine("Division"+(a / b));
+        }
+        static void Main2(string[] args)
         {
             Add a = new Add(DelegatesEx.Add1);
-            Add1(2, 3);
+            Sub b = new Sub(DelegatesEx.Sub1);
+            Mul c = new Mul(DelegatesEx.Mul1);
+            Div d = new Div(DelegatesEx.Div1);
+            a(2, 3);
+            b(4, 5);
+            c(6, 7);
+            d(2, 1);
         }
     }
 }
---
+++ Parallelism/MulticastDeigate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parallelism
+{
+    public delegate void Calculator(int a, int b);
+    class MulticastDeigate
+    {
+        public void add(int a,int b)
+        {
+            Console.WriteLine("Addition=" + (a + b));
+        }
+        public void sub(int a,int b)
+        {
+            Console.WriteLine("Subtraction=" + (a - b));
+        }
+        public void mul(int a,int b)
+        {
+            Console.WriteLine("multiply=" + (a * b));
+        }
+        public void div(int a,int b)
+        {
+            Console.WriteLine("division=" + (a / b));
+        }
+        static void Main(string[] args)
+        {
+            MulticastDeigate m = new MulticastDeigate();
+            Calculator c = new Calculator(m.add);
+            c = c + m.sub;
+            c = c + m.mul;
+            c = c + m.div;
+            c.Invoke(8, 4);
+        }
+    }
+}
