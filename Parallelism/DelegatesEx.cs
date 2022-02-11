e3d55e81b56a23f76129129b88e8d79a4bd9f2b9
Manikiran004 <kiranmani004@gmail.com>
2022-02-11 12:00:24 +0530

    delegates

---
+++ Parallelism/DelegatesEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parallelism
+{
+    public delegate void Add(int a, int b);
+    class DelegatesEx
+    {
+        public static void Add1(int a,int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        static void Main(string[] args)
+        {
+            Add a = new Add(DelegatesEx.Add1);
+            Add1(2, 3);
+        }
+    }
+}
---
+++ Parallelism/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Parallelism
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            List<int> i = Enumerable.Range(1, 10).ToList();
+            foreach (var item in i)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
