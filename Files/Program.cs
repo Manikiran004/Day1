f8df79cbf28b8d06da619b0e3b913aa397a584bb
Manikiran004 <kiranmani004@gmail.com>
2022-02-14 12:08:32 +0530

    files

---
+++ Files/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* StreamWriter s = new StreamWriter("C://Users//Dell//Desktop//Git//File-Handler//sample");
+             s.WriteLine("Hello...THis is sample file");
+             s.Flush();
+             s.Close(); */
+            StreamReader s = new StreamReader("C://Users//Dell//Desktop//Git//File-Handler//sample");
+            string s1=s.ReadLine();
+            while (s1 != null)
+            {
+                Console.WriteLine(s1);
+                s1 = s.ReadLine();
+                
+            }
+            s.Close();
+        }
+    }
+}
